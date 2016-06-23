d31543e8e5447a90300d200f78a145dcac83346b
fchristysen <fchristysen.m@gmail.com>
2016-06-23 16:47:30 +0700

    two way binding via xml expression

--- app/src/main/java/org/greenfroyo/androidmvp_bind/app/twoway/TwoWayViewModel.java
+++ app/src/main/java/org/greenfroyo/androidmvp_bind/app/twoway/TwoWayViewModel.java
@@ -13,23 +13,8 @@ import org.greenfroyo.androidmvp_bind.app._core.BaseViewModel;
 
 public class TwoWayViewModel extends BaseViewModel {
     public ObservableField<String> mText;
-    public TextWatcher mWatcher;
 
     public TwoWayViewModel(){
         this.mText = new ObservableField<>("Default Text");
-        this.mWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {}
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                if(!s.toString().equals(mText.get())){
-                    mText.set(s.toString());
-                }
-            }
-        };
     }
 }
