4eedb41720d35e2e62c6f4c3efe54fd2d0d769e3
fchristysen <fchristysen.m@gmail.com>
2016-08-08 13:15:06 +0700

    remove ice pick support

--- app/src/main/java/org/greenfroyo/androidmvp_bind/app/_core/BasePresenter.java
+++ app/src/main/java/org/greenfroyo/androidmvp_bind/app/_core/BasePresenter.java
@@ -1,12 +1,8 @@
 package org.greenfroyo.androidmvp_bind.app._core;
 
-import android.os.Bundle;
-
 import org.greenfroyo.mvp_bind.base.BaseMvpPresenter;
-import org.greenfroyo.mvp_bind.base.BaseMvpViewModel;
 import org.greenfroyo.mvp_bind.model.MvpViewModel;
 
-import icepick.Icepick;
 import rx.subscriptions.CompositeSubscription;
 
 
@@ -20,18 +16,6 @@ import rx.subscriptions.CompositeSubscription;
 public abstract class BasePresenter<VM extends MvpViewModel> extends BaseMvpPresenter<VM> {
 
     protected CompositeSubscription mSubscription = new CompositeSubscription();
-
-    @Override
-    public void onCreate(Bundle presenterState) {
-        super.onCreate(presenterState);
-        Icepick.restoreInstanceState(this, presenterState);
-    }
-
-    @Override
-    public void onSaveInstanceState(Bundle outPresenterState) {
-        super.onSaveInstanceState(outPresenterState);
-        Icepick.saveInstanceState(this, outPresenterState);
-    }
 
     @Override
     protected void onDestroy() {
