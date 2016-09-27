e2b33060bc9c6df0cad0f20afa7eca584d8c2616
fchristysen <fchristysen.m@gmail.com>
2016-09-27 11:47:01 +0700

    move pending view model property change callback to super class

--- app/src/main/java/org/greenfroyo/androidmvp_bind/app/_core/BaseViewModel.java
+++ app/src/main/java/org/greenfroyo/androidmvp_bind/app/_core/BaseViewModel.java
@@ -1,16 +1,11 @@
 package org.greenfroyo.androidmvp_bind.app._core;
 
 import android.databinding.Bindable;
-import android.databinding.Observable;
 
 import com.android.databinding.library.baseAdapters.BR;
 
 import org.greenfroyo.androidmvp_bind.app.MVPBApp;
 import org.greenfroyo.mvpb.base.BaseMvpViewModel;
-import org.greenfroyo.mvpb.view.MainPropertyChangeCallback;
-import org.parceler.Transient;
-
-import java.util.HashSet;
 
 /**
  * Created by fchristysen on 6/7/16.
@@ -22,22 +17,9 @@ import java.util.HashSet;
 
 public abstract class BaseViewModel extends BaseMvpViewModel {
     protected String mToastMessage;
-    @Transient protected OnPropertyChangedCallback mPendingPropertyChangeListener;
-    @Transient protected HashSet<Integer> mPendings = new HashSet<>();
-    @Transient protected boolean isPending = true;
 
     public BaseViewModel() {
-        mPendingPropertyChangeListener = new OnPropertyChangedCallback() {
-            @Override
-            public void onPropertyChanged(Observable observable, int i) {
-                synchronized (BaseViewModel.this) {
-                    if (isPending) {
-                        mPendings.add(i);
-                    }
-                }
-            }
-        };
-        this.addOnPropertyChangedCallback(mPendingPropertyChangeListener);
+        super();
     }
 
     @Bindable
@@ -59,28 +41,6 @@ public abstract class BaseViewModel extends BaseMvpViewModel {
 
     public boolean needToShowToast() {
         return mToastMessage != null;
-    }
-
-    @Override
-    public synchronized void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
-        super.addOnPropertyChangedCallback(callback);
-        if(callback instanceof MainPropertyChangeCallback) {
-            isPending = false;
-        }
-        synchronized (BaseViewModel.this) {
-            for (Integer i : mPendings) {
-                callback.onPropertyChanged(this, i);
-            }
-            mPendings.clear();
-        }
-    }
-
-    @Override
-    public synchronized void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
-        super.removeOnPropertyChangedCallback(callback);
-        if(callback instanceof MainPropertyChangeCallback) {
-            isPending = true;
-        }
     }
 
 
--- mvpb/src/main/java/org/greenfroyo/mvpb/base/BaseMvpViewModel.java
+++ mvpb/src/main/java/org/greenfroyo/mvpb/base/BaseMvpViewModel.java
@@ -3,13 +3,12 @@ package org.greenfroyo.mvpb.base;
 import android.databinding.BaseObservable;
 import android.databinding.Observable;
 
-
 import org.greenfroyo.mvpb.model.MvpViewModel;
+import org.greenfroyo.mvpb.view.MainPropertyChangeCallback;
 import org.parceler.Parcel;
 import org.parceler.Transient;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 
 /**
  * Created by fchristysen on 7/17/16.
@@ -18,9 +17,43 @@ import java.util.List;
 @Parcel
 public class BaseMvpViewModel extends BaseObservable implements MvpViewModel {
 
-    public BaseMvpViewModel() {
+    @Transient protected OnPropertyChangedCallback mPendingPropertyChangeListener;
+    @Transient protected HashSet<Integer> mPendings = new HashSet<>();
+    @Transient protected boolean isPending = true;
 
+    public BaseMvpViewModel() {
+        mPendingPropertyChangeListener = new OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable observable, int i) {
+                synchronized (BaseMvpViewModel.this) {
+                    if (isPending) {
+                        mPendings.add(i);
+                    }
+                }
+            }
+        };
+        this.addOnPropertyChangedCallback(mPendingPropertyChangeListener);
     }
 
+    @Override
+    public synchronized void addOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        super.addOnPropertyChangedCallback(callback);
+        if(callback instanceof MainPropertyChangeCallback) {
+            isPending = false;
+        }
+        synchronized (BaseMvpViewModel.this) {
+            for (Integer i : mPendings) {
+                callback.onPropertyChanged(this, i);
+            }
+            mPendings.clear();
+        }
+    }
 
+    @Override
+    public synchronized void removeOnPropertyChangedCallback(OnPropertyChangedCallback callback) {
+        super.removeOnPropertyChangedCallback(callback);
+        if(callback instanceof MainPropertyChangeCallback) {
+            isPending = true;
+        }
+    }
 }
